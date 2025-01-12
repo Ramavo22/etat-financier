518ada3c3b03052e7c55491e80341c72e7fdc4ee
Pokaneliot <161971448+Pokaneliot@users.noreply.github.com>
2025-01-12 22:01:52 +0300

    test

--- etat-financier/src/main/resources/sql/database.sql
+++ etat-financier/src/main/resources/sql/database.sql
@@ -27,3 +27,86 @@ CREATE TABLE transaction_financiere (
     date_transaction DATE NOT NULL,          -- Date et heure de la transaction
     FOREIGN KEY (compte_financier_id) REFERENCES compte_financier(id) 
 );
+
+
+CREATE OR REPLACE FUNCTION get_enfants_recursive(nom_rubrique VARCHAR)
+RETURNS TABLE(id INT, nom VARCHAR, type_compte INT, parent_id INT) AS
+$$
+BEGIN
+    RETURN QUERY
+    WITH RECURSIVE enfants AS (
+        SELECT c.id, c.nom, c.type_compte, c.parent_id
+        FROM compte_financier c
+        WHERE c.nom = nom_rubrique
+        
+        UNION ALL
+        
+        SELECT c.id, c.nom, c.type_compte, c.parent_id
+        FROM compte_financier c
+        INNER JOIN enfants e ON c.parent_id = e.id
+    )
+    SELECT e.id, e.nom, e.type_compte, e.parent_id
+    FROM enfants e;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT * FROM get_enfants_recursive('Actifs courants');
+
+
+. Actifs Non Courants :
+Immobilisations incorporelles : Somme des comptes associés.
+
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations incorporelles') e
+    ON t.compte_financier_id = e.id;
+
+Immobilisations corporelles : Somme des comptes associés.
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations corporelles') e
+    ON t.compte_financier_id = e.id;
+
+Immobilisations en cours : Somme des comptes associés.
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations en cours') e
+    ON t.compte_financier_id = e.id;
+
+Immobilisations financières : Somme des comptes associés.
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Autres immobilisations financières') e
+    ON t.compte_financier_id = e.id;
+
+SELECT SUM(t.montant) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Actifs Non courant') e
+    ON t.compte_financier_id = e.id;
+
+2. Actifs Courants :
+Stocks et en cours : Somme des comptes associés (stocks, matières premières, produits, etc.).
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations incorporelles') e
+    ON t.compte_financier_id = e.id;
+
+Créances et emplois assimilés : Somme des comptes de créances (clients, autres débiteurs).
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations incorporelles') e
+    ON t.compte_financier_id = e.id;
+
+Trésorerie et équivalents de trésorerie : Somme des comptes de trésorerie (banques, caisse).
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations incorporelles') e
+    ON t.compte_financier_id = e.id;
+
+SELECT COALESCE(SUM(t.montant), 0) AS somme
+FROM transaction_financiere t
+JOIN get_enfants_recursive('Immobilisations incorporelles') e
+    ON t.compte_financier_id = e.id;
+
+
